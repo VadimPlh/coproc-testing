649b31344d6a782026aa9d6712064e10f6c92a8f
Vadim Plakhtinskiy <18518081+VadimPlh@users.noreply.github.com>
2021-11-15 13:42:55 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -8,7 +8,7 @@ export TIMEOUT=1200000000
 
 export MESSAGES_COUNT=2000 
 
-export BROKERS="44.242.148.141:9092,34.223.48.244:9092,44.234.62.1:9092"
-export RPK_PATH="/home/vadim/redpanda/vbuild/go/linux/bin/rpk"
-export producer="../../kafka_2.12-3.0.0/bin/kafka-producer-perf-test.sh"
-export consumer="../../kafka_2.12-3.0.0/bin/kafka-consumer-perf-test.sh"
+export BROKERS=""
+export RPK_PATH=""
+export producer=""
+export consumer=""
